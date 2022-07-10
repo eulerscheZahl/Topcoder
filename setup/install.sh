9f07de70f521945416d5820dfb0696494f3dbb31
eulerscheZahl <eulerschezahl@yandex.com>
2022-07-10 10:53:38 +0200

    contest setup

--- setup/install.sh
+++ setup/install.sh
@@ -5,6 +5,13 @@ sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 apt update
+sudo get install gpg -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+apt update
 apt install i3 dmenu -y
 mkdir ~/.config
 mkdir ~/.config/i3
