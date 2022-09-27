6dc922648818350ffd14ee964e39cc49e53cfb60
eulerscheZahl <eulerschezahl@yandex.com>
2022-09-27 11:00:43 +0200

    update leaderboard script

--- setup/MM/leaderboard.py
+++ setup/MM/leaderboard.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
- 
+
 import os
+minimize = False
 best_scores = {}
 file_scores = {}
 
@@ -15,16 +16,21 @@ for file in os.listdir('scores/'):
         l = l.replace('= ', '').replace(', ', ' ').split(' ')
         seed = int(l[1])
         score = float(l[3])
-        if not seed in best_scores: best_scores[seed] = score
-        best_scores[seed] = max(best_scores[seed], score)
-        #best_scores[seed] = min(best_scores[seed], score)
         file_scores[file][seed] = score
+        if score == -1: continue
+        if not seed in best_scores: best_scores[seed] = score
+        if minimize: best_scores[seed] = min(best_scores[seed], score)
+        else: best_scores[seed] = max(best_scores[seed], score)
+
+keys = sorted(list(best_scores.keys()))
+print('   '.join(str(k)+':'+str(best_scores[k]) for k in keys))
 
 for f in sorted(file_scores):
     if len(file_scores[f]) == 0: continue
     score = 0
     for seed in file_scores[f]:
-        score += file_scores[f][seed] / best_scores[seed]
-        #score += best_scores[seed] / file_scores[f][seed]
+        if file_scores[f][seed] == -1: continue
+        if minimize: score += best_scores[seed] / file_scores[f][seed]
+        else: score += file_scores[f][seed] / best_scores[seed]
     print(f, score, '...', score * len(best_scores) / len(file_scores[f]))
 
